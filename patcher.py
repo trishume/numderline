0e1f05f24930a78a664ba5efba44b50aef7b9531
Tristan Hume <tris.hume@gmail.com>
2019-09-29 14:01:22 -0400

    Progress but hit fontforge bug

--- patcher.py
+++ patcher.py
@@ -32,6 +32,7 @@ def get_argparser(ArgumentParser=argparse.ArgumentParser):
 
 
 FONT_NAME_RE = re.compile(r'^([^-]*)(?:(-.*))?$')
+NUM_DIGIT_COPIES = 6
 
 def gen_feature(digit_names):
     feature = """
@@ -41,47 +42,61 @@ languagesystem cyrl dflt;
 languagesystem grek dflt;
 languagesystem kana dflt;
 @digits=[{digit_names}];
+{nds}
+{allnds}
+{allnds_nxt}
 
 feature calt {{
-  sub two three by nine;
+  sub @digits by @nd0;
 }} calt;
 """[1:]
-    feature = feature.format(digit_names=' '.join(digit_names))
+
+    nds = [' '.join(['nd{}.{}'.format(i,j) for j in range(10)]) for i in range(NUM_DIGIT_COPIES)]
+    nds = ['@nd{}=[{}];'.format(i,nds[i]) for i in range(NUM_DIGIT_COPIES)]
+    nds = "\n".join(nds)
+
+    allnds = ['nd{}.{}'.format(i,j) for i in range(NUM_DIGIT_COPIES) for j in range(10)]
+    allnds = "@allnds=[{}];".format(' '.join(allnds))
+
+    allnds_nxt = ['nd{}.{}'.format((i+1)%NUM_DIGIT_COPIES,j) for i in range(NUM_DIGIT_COPIES) for j in range(10)]
+    allnds_nxt = "@allnds_nxt=[{}];".format(' '.join(allnds_nxt))
+
+    feature = feature.format(digit_names=' '.join(digit_names), nds=nds, allnds=allnds, allnds_nxt=allnds_nxt)
     with open('mods.fea', 'w') as f:
         f.write(feature)
 
 
-def patch_one_font(target_font, rename_font=True):
-    target_font_em_original = target_font.em
-    target_font.em = 2048
-    target_font.encoding = 'ISO10646'
+def patch_one_font(font, rename_font=True):
+    font_em_original = font.em
+    font.em = 2048
+    font.encoding = 'ISO10646'
 
     # Rename font
     if rename_font:
-        target_font.familyname += ' with Numderline'
-        target_font.fullname += ' with Numderline'
-        fontname, style = FONT_NAME_RE.match(target_font.fontname).groups()
-        target_font.fontname = fontname + 'WithNumderline'
+        font.familyname += ' with Numderline'
+        font.fullname += ' with Numderline'
+        fontname, style = FONT_NAME_RE.match(font.fontname).groups()
+        font.fontname = fontname + 'WithNumderline'
         if style is not None:
-            target_font.fontname += style
-        target_font.appendSFNTName(
-            'English (US)', 'Preferred Family', target_font.familyname)
-        target_font.appendSFNTName(
-            'English (US)', 'Compatible Full', target_font.fullname)
+            font.fontname += style
+        font.appendSFNTName(
+            'English (US)', 'Preferred Family', font.familyname)
+        font.appendSFNTName(
+            'English (US)', 'Compatible Full', font.fullname)
 
     target_bb = [0, 0, 0, 0]
-    target_font_width = 0
+    font_width = 0
 
     # Find the biggest char width and height in the Latin-1 extended range and
     # the box drawing range This isn't ideal, but it works fairly well - some
     # fonts may need tuning after patching.
     for cp in chain(range(0x00, 0x17f), range(0x2500, 0x2600)):
         try:
-            bbox = target_font[cp].boundingBox()
+            bbox = font[cp].boundingBox()
         except TypeError:
             continue
-        if not target_font_width:
-            target_font_width = target_font[cp].width
+        if not font_width:
+            font_width = font[cp].width
         if bbox[0] < target_bb[0]:
             target_bb[0] = bbox[0]
         if bbox[1] < target_bb[1]:
@@ -91,59 +106,39 @@ def patch_one_font(target_font, rename_font=True):
         if bbox[3] > target_bb[3]:
             target_bb[3] = bbox[3]
 
-    # Find source and target size difference for scaling
-    # x_ratio = (target_bb[2] - target_bb[0]) / (source_bb[2] - source_bb[0])
-    # y_ratio = (target_bb[3] - target_bb[1]) / (source_bb[3] - source_bb[1])
-    # scale = psMat.scale(x_ratio, y_ratio)
+    font.em = font_em_original
 
-    # Find source and target midpoints for translating
-    # x_diff = target_bb[0] - source_bb[0]
-    # y_diff = target_bb[1] - source_bb[1]
-    # translate = psMat.translate(x_diff, y_diff)
-    # transform = psMat.compose(scale, translate)
-
-    # # Create new glyphs from symbol font
-    # for source_glyph in source_font.glyphs():
-    #     if source_glyph == source_font['block']:
-    #         # Skip the symbol font block glyph
-    #         continue
-
-    #     # Select and copy symbol from its encoding point
-    #     source_font.selection.select(source_glyph.encoding)
-    #     source_font.copy()
-
-    #     # Select and paste symbol to its unicode code point
-    #     target_font.selection.select(source_glyph.unicode)
-    #     target_font.paste()
-
-    #     # Transform the glyph
-    #     target_font.transform(transform)
-
-    #     # Reset the font's glyph width so it's still considered monospaced
-    #     target_font[source_glyph.unicode].width = target_font_width
-
-    target_font.em = target_font_em_original
-
-    # FIXME: Menlo and Meslo font do have these substitutes, but U+FB01 and
-    #        U+FB02 still do not show up for fi and fl.
-    # target_font[0xFB01].removePosSub('*')  # fi ligature
-    # target_font[0xFB02].removePosSub('*')  # fl ligature
-
-    digit_names = [target_font[code].glyphname for code in range(ord('0'),ord('9')+1)]
+    digit_names = [font[code].glyphname for code in range(ord('0'),ord('9')+1)]
+    test_names = [font[code].glyphname for code in range(ord('A'),ord('J')+1)]
     print(digit_names)
-    gen_feature(digit_names)
-    target_font.mergeFeature('mods.fea')
 
-    # for glyph in target_font:
-    #     print(glyph)
+    # 0xE900 starts an area spanning until 0xF000 that as far as I can tell nothing
+    # popular uses. I checked the Apple glyph browser and Nerd Font.
+    # Uses an array because of python closure capture semantics
+    encoding_alloc = [0xE900]
+    def make_copy(loc, to_name):
+        encoding = encoding_alloc[0]
+        font.selection.select(loc)
+        font.copy()
+        font.selection.select(encoding)
+        font.paste()
+        font[encoding].glyphname = to_name
+        encoding_alloc[0] += 1
+
+    for copy_i in range(0,NUM_DIGIT_COPIES):
+        for digit_i in range(0,10):
+            make_copy(test_names[copy_i], 'nd{}.{}'.format(copy_i,digit_i))
+
+    gen_feature(digit_names)
+    font.mergeFeature('mods.fea')
 
     # Generate patched font
-    # extension = os.path.splitext(target_font.path)[1]
+    # extension = os.path.splitext(font.path)[1]
     # if extension.lower() not in ['.ttf', '.otf']:
     #     # Default to OpenType if input is not TrueType/OpenType
     #     extension = '.otf'
     extension = '.otf'
-    target_font.generate('out/{0}{1}'.format(target_font.fullname, extension))
+    font.generate('out/{0}{1}'.format(font.fullname, extension))
 
 
 def patch_fonts(target_files, rename_font=True):
